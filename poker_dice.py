7e633fd48e05f2ce4c6afe6ce1d81c5066191413
Matthew <mldelaney94@gmail.com>
2020-03-14 01:15:34 +1100

    Finalised play() function

--- poker_dice.py
+++ poker_dice.py
@@ -1,3 +1,6 @@
+""" Simulates games of poker dice, where the dice have no suits, and only
+NINE through ACE is possible. Written Matthew Delaney March 2020 """
+
 from random import randint, randrange
 from itertools import groupby
 
@@ -10,38 +13,69 @@ FOUR_KIND = 5
 FIVE_KIND = 6
 BUST = 7
 
-NINE = 0
-TEN = 1
-JACK = 2
-QUEEN = 3
-KING = 4
-ACE = 5
+ACE = 0
+KING = 1
+QUEEN = 2
+JACK = 3
+TEN = 4
+NINE = 5
 
 def play():
-    pass
-    # REPLACE PASS ABOVE WITH YOUR CODE
+    result_dict = {ONE_PAIR: 'One pair', TWO_PAIR: 'Two pair',
+                   THREE_KIND: 'Three of a kind', FULL_HOUSE: 'Full house',
+                   STRAIGHT: 'Straight', FOUR_KIND: 'Four of a kind',
+                   FIVE_KIND: 'Five of a kind'}
+    turn_dict = {2: 'second', 3: 'third'}
+
+    hand = generate_hand(5)
+    for i in range(2, 4):
+        print("The roll is: " + ' '.join(hand))
+        print("It is a " + result_dict[determine_hand(hand)])
+        keep_list = input('Which dice do you want to keep for the ' +
+            turn_dict[i] + ' roll? ').split()
+
+        while not legal_keep_list(keep_list, hand):
+            print('That is not possible, try again!')
+            inp = input('Which dice do you want to keep for the ' +
+                turn_dict[i] + ' roll? ')
+            keep_list = inp.split()
+
+        if len(keep_list) == 0:
+            hand = generate_hand(5)
+        elif 'all' in keep_list or 'All' in keep_list:
+            print("Ok, done.")
+            return
+        else:
+            keep_list.extend(generate_hand(5-len(keep_list)))
+            hand = keep_list
+    
+    print("The roll is: " + ' '.join(hand))
+    print("It is a " + result_dict[determine_hand(hand)])
+    return
 
 def simulate(n):
     hand_percentage = {ONE_PAIR: 0, TWO_PAIR: 0, THREE_KIND: 0, FULL_HOUSE: 0,
                        STRAIGHT: 0, FOUR_KIND: 0, FIVE_KIND: 0}
     for i in range(n):
-        hand = generate_hand()
+        hand = generate_hand(5)
         hand_percentage[determine_hand(hand)] += 1
 
     simulation_to_string(n, hand_percentage)
 
 # DEFINE OTHER FUNCTIONS
-def generate_hand():
+def generate_hand(num_cards):
+    """ generates a list num_cards long of random cards """
     hand = []
     hand_dict = {NINE: '9', TEN: '10', JACK: 'Jack', QUEEN: 'Queen',
                  KING: 'King', ACE: 'Ace'}
-    'King'
-    for i in range(5):
-        hand.append(hand_dict[randrange(NINE, ACE)])
+
+    for i in range(num_cards):
+        hand.append(hand_dict[randrange(ACE, NINE)])
 
     return hand
 
 def determine_hand(dice):
+    """ Determines the rank of a legal hand """
     straight_1 = {'Ace', 'King', 'Queen', 'Jack', '10'}
     straight_2 = {'King', 'Queen', 'Jack', '10', '9'}
     if set(dice) == straight_1 or set(dice) == straight_2:
@@ -70,6 +104,7 @@ def determine_hand(dice):
         return TWO_PAIR
 
 def simulation_to_string(n, results):
+    """ Prints the results of the simulation """
     results_fivekind = 100 * (results[FIVE_KIND] / n)
     results_fourkind = 100 * (results[FOUR_KIND] / n)
     results_threekind = 100 * (results[THREE_KIND] / n)
@@ -85,4 +120,19 @@ def simulation_to_string(n, results):
     print("Two pair: " + '%.2f' % results_twopair + "%")
     print("One pair: " + '%.2f' % results_onepair + "%")
 
-simulate(1000000)
+def legal_keep_list(keep_list, orig_hand):
+    if len(keep_list) == 0:
+        return 1
+    if len(keep_list) > 5:
+        return 0
+    legal_alls = ['all', 'All']
+    if len(keep_list) == 1 and keep_list[0] in legal_alls:
+        return 1
+
+    can_contain = ['Ace', 'King', 'Queen', 'Jack', '10', '9']
+    for item in keep_list:
+        if item not in can_contain or item not in orig_hand:
+            return 0
+    return 1
+
+play()
