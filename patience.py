0056cd474a8e2212156334f467d231bcf0a72c01
Matthew <mldelaney94@gmail.com>
2020-03-14 00:24:22 +1100

    enabled simulation of hands in poker_dice.py

--- patience.py
+++ patience.py
@@ -1,7 +1,34 @@
+""" Edited by Matthew Delaney. Simulates a game of 'patience', whereby the
+player has 52 facedown cards organised in a circle of stacks of 4, with a stack
+of 4 in the centre """
+
 from random import seed, shuffle
 
+CARD_TO_UNICODE = {
+        'S1': chr(0x1F0A1), 'S2': chr(0x1F0A2), 'S3': chr(0x1F0A3),
+        'S4': chr(0x1F0A4), 'S5': chr(0x1F0A5), 'S6': chr(0x1F0A6),
+        'S7': chr(0x1F0A7), 'S8': chr(0x1F0A8), 'S9': chr(0x1F0A9),
+        'S10': chr(0x1F0AA), 'Sj': chr(0x1F0AB), 'Sq': chr(0x1F0AD),
+        'Sk': chr(0x1F0AE),
+        'H1': chr(0x1F0B1), 'H2': chr(0x1F0B2), 'H3': chr(0x1F0B3),
+        'H4': chr(0x1F0B4), 'H5': chr(0x1F0B5), 'H6': chr(0x1F0B6),
+        'H7': chr(0x1F0B7), 'H8': chr(0x1F0B8), 'H9': chr(0x1F0B9),
+        'H10': chr(0x1F0BA), 'Hj': chr(0x1F0BB), 'Hq': chr(0x1F0BD),
+        'Hk': chr(0x1F0BE),
+        'D1': chr(0x1F0C1), 'D2': chr(0x1F0C2), 'D3': chr(0x1F0C3),
+        'D4': chr(0x1F0C4), 'D5': chr(0x1F0C5), 'D6': chr(0x1F0C6),
+        'D7': chr(0x1F0C7), 'D8': chr(0x1F0C8), 'D9': chr(0x1F0C9),
+        'D10': chr(0x1F0CA), 'Dj': chr(0x1F0CB), 'Dq': chr(0x1F0CD),
+        'Dk': chr(0x1F0CE),
+        'C1': chr(0x1F0D1), 'C2': chr(0x1F0D2), 'C3': chr(0x1F0D3),
+        'C4': chr(0x1F0D4), 'C5': chr(0x1F0D5), 'C6': chr(0x1F0D6),
+        'C7': chr(0x1F0D7), 'C8': chr(0x1F0D8), 'C9': chr(0x1F0D9),
+        'C0': chr(0x1F0DA), 'Cj': chr(0x1F0DB), 'Cq': chr(0x1F0DD),
+        'Ck': chr(0x1F0DE)
+        }
 
 def generate_dial_and_centre(for_seed):
+    """ Randomly generates a deck of cards and arranges it into 13 bundles """
     colours = 'CDHS' # Clubs, Diamonds, Hearts, Spades
     #                                            jacks, queens, kings
     ranks = list(str(x) for x in range(1, 11)) + list('jqk')
@@ -14,12 +41,14 @@ def generate_dial_and_centre(for_seed):
     return dial, [cards[12 + 13 * j] for j in range(4)]
 
 def initial_hour(hour, dial):
-    pass
+    """ Prints cards from dial using key hour """
+    for card in dial[hour]:
+        print('hidden'+CARD_TO_UNICODE[card], end=' ')
     # REPLACE PASS ABOVE WITH YOUR CODE
 
 def hour_after_playing_from_beginning_for_at_most(hour, nb_of_steps, dial,
-                                                  centre
-                                                 ):
+                                                  centre):
+
     print()
     # REPLACE PRINT() ABOVE WITH YOUR CODE
 
@@ -28,3 +57,5 @@ def kings_at_end_of_game(dial, centre):
     # REPLACE PRINT() ABOVE WITH YOUR CODE
 
 # POSSIBLY DEFINE OTHER FUNCTIONS
+clock, centre = generate_dial_and_centre(0)
+initial_hour(12, clock)
--- poker_dice.py
+++ poker_dice.py
@@ -1,13 +1,88 @@
-from random import randint
+from random import randint, randrange
+from itertools import groupby
 
+ONE_PAIR = 0
+TWO_PAIR = 1
+THREE_KIND = 2
+FULL_HOUSE = 3
+STRAIGHT = 4
+FOUR_KIND = 5
+FIVE_KIND = 6
+BUST = 7
+
+NINE = 0
+TEN = 1
+JACK = 2
+QUEEN = 3
+KING = 4
+ACE = 5
 
 def play():
     pass
     # REPLACE PASS ABOVE WITH YOUR CODE
 
 def simulate(n):
-    pass
-    # REPLACE PASS ABOVE WITH YOUR CODE
+    hand_percentage = {ONE_PAIR: 0, TWO_PAIR: 0, THREE_KIND: 0, FULL_HOUSE: 0,
+                       STRAIGHT: 0, FOUR_KIND: 0, FIVE_KIND: 0}
+    for i in range(n):
+        hand = generate_hand()
+        hand_percentage[determine_hand(hand)] += 1
 
+    simulation_to_string(n, hand_percentage)
 
 # DEFINE OTHER FUNCTIONS
+def generate_hand():
+    hand = []
+    hand_dict = {NINE: '9', TEN: '10', JACK: 'Jack', QUEEN: 'Queen',
+                 KING: 'King', ACE: 'Ace'}
+    'King'
+    for i in range(5):
+        hand.append(hand_dict[randrange(NINE, ACE)])
+
+    return hand
+
+def determine_hand(dice):
+    straight_1 = {'Ace', 'King', 'Queen', 'Jack', '10'}
+    straight_2 = {'King', 'Queen', 'Jack', '10', '9'}
+    if set(dice) == straight_1 or set(dice) == straight_2:
+        return STRAIGHT
+
+    num_unique_results = 0
+    possible_three_kind = 0
+    for result, num_results in groupby(sorted(dice)):
+        s = len(list(num_results))
+        if s == 5:
+            return FIVE_KIND
+        elif s == 4:
+            return FOUR_KIND
+        elif s == 3:
+            possible_three_kind = 1
+        num_unique_results += 1
+    if num_unique_results == 2:
+        return FULL_HOUSE
+    elif num_unique_results == 3 and possible_three_kind == 1:
+        return THREE_KIND
+    elif num_unique_results == 4:
+        return ONE_PAIR
+    elif num_unique_results == 5:
+        return BUST
+    else:
+        return TWO_PAIR
+
+def simulation_to_string(n, results):
+    results_fivekind = 100 * (results[FIVE_KIND] / n)
+    results_fourkind = 100 * (results[FOUR_KIND] / n)
+    results_threekind = 100 * (results[THREE_KIND] / n)
+    results_twopair = 100 * (results[TWO_PAIR] / n)
+    results_onepair = 100 * (results[ONE_PAIR] / n)
+    results_straight = 100 * (results[STRAIGHT] / n)
+    results_fullhouse = 100 * (results[FULL_HOUSE] / n)
+    print("Five of a kind : " + '%.2f' % results_fivekind + "%")
+    print("Four of a kind: " + '%.2f' % results_fourkind + "%")
+    print("Full house: " + '%.2f' % results_fullhouse + "%")
+    print("Straight: " + '%.2f' % results_straight + "%")
+    print("Three of a kind: " + '%.2f' % results_threekind + "%")
+    print("Two pair: " + '%.2f' % results_twopair + "%")
+    print("One pair: " + '%.2f' % results_onepair + "%")
+
+simulate(1000000)
